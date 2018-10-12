dfc570ba560531d1defc9d6217adbdafa69514a6
niweijian <niweijian@kingsoft.com>
2018-10-12 10:06:21 +0800

    除去robot meta 添加adsense

--- console/nuxt.config.js
+++ console/nuxt.config.js
@@ -9,8 +9,8 @@ module.exports = {
     title: 'Pipe',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'robots', content: 'none' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      // { name: 'robots', content: 'none' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
